408f2e3b7fef370a2199823423900dcd27323db5
GitVito <victoraraujobaffa0901@gmail.com>
2022-11-21 17:13:02 -0300

    perfilQuaseProonto
    Falta apenas inserir uma imagem, e um pequeno bloco de código

---
+++ Site/js/perfil..js
@@ -0,0 +1,6 @@
+var imgUpload = document.getElementById('imgFundoChat').value;
+
+function mostrarImg(){
+    var showImgUp = document.getElementById('showImgUp');
+    showImgUp.src= 'imgUpload';
+}
