ed79762f7b478a68dc3b614302fa654f23ee9262
RafaellaTomassetti <tomassettirafaella@gmail.com>
2022-11-23 10:46:59 -0300

    aaaaa
    aaaa

--- Site/firebase/module.js
+++ Site/firebase/module.js
@@ -23,7 +23,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-document.getElementsByTagName("button")[btnEntrarDeVez].addEventListener('click', function(){
+document.getElementsByTagName("button")[0,1,2,4,5].addEventListener('click', function(){
 
     email = document.getElementById("digitoCpfEmail").value 
     senha = document.getElementById("digitoSenha").value 
