29c7006080a7c771fe1a89cee299c6629725d807
Cardelis <102964288+Alinebaatista@users.noreply.github.com>
2022-11-29 11:11:47 -0300

    commit
    29/11: 11:11

--- Site/js/app.js
+++ Site/js/app.js
@@ -85,3 +85,123 @@ document.querySelector('#next-year').onclick = () => {
 }
 
 
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const sTítulo = document.querySelector('#m-Título')
+const sInício = document.querySelector('#m-Início')
+const sFim = document.querySelector('#m-Fim')
+const sDescricao = document.querySelector('#m-Descrição')
+const sHorario = document.querySelector('#m-Horário')
+const sTurma = document.querySelector('#m-Turma')
+const sMateria = document.querySelector('#m-Matéria')
+const btnSalvar = document.querySelector('#btnSalvar')
+
+let itens
+let id
+
+function openModal(edit = false, index = 0) {
+    modal.classList.add('active')
+
+    modal.onclick = e => {
+        if (e.target.className.indexOf('modal-container') !== -1) {
+            modal.classList.remove('active')
+        }
+    }
+
+    if (edit) {
+        sNome.value = itens[index].Título
+        sFuncao.value = itens[index].Início
+        sSetor.value = itens[index].Fim
+        sDescrição.value = itens[index].Descrição
+        sHorário.value = itens[index].Horário
+        sTurma.value = itens[index].Turma
+        sMatéria.value = itens[index].sMatéria
+ 
+        id = index
+    } else {
+        sTítulo.value = ''
+        sInício.value = ''
+        sFim.value = ''
+        sDescrição.value = ''
+        sHorário.value = ''
+        sTurma.value = ''
+        sMatéria.value = ''
+       
+    }
+}
+
+function editItem(index) {
+    openModal(true, index)
+}
+
+function deleteItem(index) {
+    itens.splice(index, 1)
+    setItensBD()
+    loadItens()
+}
+
+function insertItem(item, index) {
+    let tr = document.createElement('tr')
+
+    tr.innerHTML = `
+     <td>${item.Título}</td>
+     <td>${item.Início}</td>
+     <td>${item.Fim}</td>
+     <td>${item.Descrição}</td>
+     <td>${item.Horário}</td>
+     <td>${item.Turma}</td>
+     <td>${item.Matéria}</td>
+    
+     <td class="acao">
+        <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
+    </td>
+    <td class="acao">
+        <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+    </td>
+    `
+    tbody.appendChild(tr)
+}
+
+btnSalvar.onclick = e => {
+    if (sTítulo.value == '' || sInício.value == '' ||  sFim.value == '' || sDescrição.value == '' || sHorário.value == '' ||  sTurma.value == '' ||  sMatéria.value == '' ) {
+        return
+    }
+
+    e.preventDefault();
+
+    if (id !== undefined) {
+        itens[id].Titulo = sTítulo.value
+        itens[id].Início = sInício.value
+        itens[id].Fim = sFim.value
+        itens[id].Descrição = sDescrição.value
+        itens[id].Horário = sHorário.value
+        itens[id].Turma = sTurma.value
+        itens[id].Matéria = sMatéria.value
+    
+    } else {
+        itens.push({'Título': sTítulo.value, 'Início': sInício.value, 'Fim': sFim.value ,'Descrição': sDescrição.value, 'Horário': sHorário.value, 'Turma': sTurma.value, 'Matéria': sMatéria.value, })
+    }
+
+    setItensBD()
+
+    modal.classList.remove('active')
+    loadItens()
+     
+}
+
+function loadItens() {
+    itens = getItensBD()
+    tbody.innerHTML = ''
+    itens.forEach((item, index) => {
+        insertItem(item, index)
+    })
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ??[]
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
+loadItens()
+
+
+
+
