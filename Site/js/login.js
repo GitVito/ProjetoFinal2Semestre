1c2c06999b06c2652bd67a28c4e98b69b6fcf68e
RafaellaTomassetti <tomassettirafaella@gmail.com>
2022-12-12 08:26:55 -0300

    )()()()()()()(*(*((*(*(*&¨&¨*%¨¨%&%$

--- Site/js/login.js
+++ Site/js/login.js
@@ -49,22 +49,3 @@ function apagarDigito(){
 }
 
 
-/*Show-Password*/
-
-$(document).ready(function(){
-    $('#showPassword').on('click', function(){
-      
-      var passwordField = $('#password');
-      var passwordFieldType = passwordField.attr('type');
-      if(passwordFieldType == 'password')
-      {
-          passwordField.attr('type', 'text');
-          $(this).val('Hide');
-      } else {
-          passwordField.attr('type', 'password');
-          $(this).val('Show');
-      }
-    });
-  });
-
-  /*--------*/
