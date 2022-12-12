892da59ca815f407a937ba6fd43ca1fdd9e0da67
RafaellaTomassetti <tomassettirafaella@gmail.com>
2022-12-12 08:13:16 -0300

    aaaaaaaaaa
    aaaaaaaaaaaaaaaaaaaaaaaaa

--- Site/firebase/module.js
+++ Site/firebase/module.js
@@ -1,19 +1,3 @@
-
-//Firebase config.....
-const firebaseConfig = {
-  apiKey: "AIzaSyB0mRJtewMmLrtjVhKwO5jfVFQ4lY4zOFI",
-  authDomain: "seed-f091d.firebaseapp.com",
-  databaseURL: "https://seed-f091d-default-rtdb.firebaseio.com",
-  projectId: "seed-f091d",
-  storageBucket: "seed-f091d.appspot.com",
-  messagingSenderId: "467421316797",
-  appId: "1:467421316797:web:b8ae86d14e959af2b519d4",
-  measurementId: "G-HMX1XZ8VG7"
-};
-   
-// inicia o firebase 
-const app = initializeApp(firebaseConfig);
-
 function entrarProfessor() {
   firebase.auth().signInWithEmailAndPassword(
       form.email().value, form.password().value
